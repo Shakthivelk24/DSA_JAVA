3e1dc719cf908c9d91a6c98d67719827ee524647
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-09-08 18:33:28 +0530

    Variable of Java

---
+++ Java/Basic/variable.java
@@ -0,0 +1,17 @@
+class variable
+{
+    public static void main(String args[])
+    {
+        int a=10; //integer variable
+        float b=20.5f; //float variable
+        char c='A'; //character variable
+        String d="Hello"; //string variable
+        boolean e=true; //boolean variable
+
+        System.out.println("Integer variable: " + a);
+        System.out.println("Float variable: " + b);
+        System.out.println("Character variable: " + c);
+        System.out.println("String variable: " + d);
+        System.out.println("Boolean variable: " + e);
+    }
+}
