8122f7d232f6bc2c67314ad4d0158c81f13463c6
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-09-11 20:20:47 +0530

    Method in Java

---
+++ Java/Basic/Method.java
@@ -0,0 +1,12 @@
+public class Method {
+    public void printMessage() {
+        System.out.println("Hello, This is a method !");
+    }
+    public static void main(String[] args) {
+        // Create an instance of the Method class
+        Method obj = new Method();
+        
+        // Calling the method
+        obj.printMessage(); 
+    }
+}
