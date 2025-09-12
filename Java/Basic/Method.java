1e2e7f2c78643881cb5911f8e40b02afcf3c07de
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-09-12 18:28:34 +0530

    Some problem

--- Java/Basic/Method.java
+++ Java/Basic/Method.java
@@ -3,10 +3,12 @@ public class Method {
         System.out.println("Hello, This is a method !");
     }
     public static void main(String[] args) {
+        
         // Create an instance of the Method class
         Method obj = new Method();
         
         // Calling the method
-        obj.printMessage(); 
+        obj.printMessage();
+
     }
 }
