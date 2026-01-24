1e9928bea6efbbfd5e6be9477d4b498f9b2998e3
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-24 16:59:48 +0530

    Add Inverted Triangle pattern implementation

---
+++ Java/Pattern/InvertedTriangleR.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InvertedTriangleR {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the Number of Rows : ");
+        n = input.nextInt();
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(num);
+            }
+            num++;
+            System.out.println();
+        }
+    }
+}
+// Output
+// Enter the Number of Rows : 
+// 4
+// 1111
+// 222
+// 33
+// 4
