c56febfa93c200067249558dc46755909faa64ad
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-13 21:34:13 +0530

    Add Enumeration class with Day enum and main method

---
+++ Java/Basic/Enumeration.java
@@ -0,0 +1,13 @@
+public class Enumeration {
+    enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY,
+        FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public static void main(String[] args) {
+        Day today = Day.MONDAY;
+
+        System.out.println(today);
+    }
+}
+// Output: MONDAY
