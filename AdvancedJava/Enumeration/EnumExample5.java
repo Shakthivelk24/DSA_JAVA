4c1c7112c1cedcc749a3894ee768f553bceab151
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-03-07 20:44:40 +0530

    Enumeration using the methods values() and valueOf()

---
+++ AdvancedJava/Enumeration/EnumExample5.java
@@ -0,0 +1,23 @@
+package AdvancedJava.Enumeration;
+
+/* values() and valueOf()*/
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE;
+}
+
+public class EnumExample5 {
+    public static void main(String[] args) {
+        System.out.println("Color Constants:");
+		// use values()
+		Color colors[] = Color.values();
+		for(Color cc : colors)
+			System.out.println(cc);
+
+		// use valueOf()
+		Color c = Color.valueOf("RED");
+		System.out.println("Color contains " + c);
+    }
+}
