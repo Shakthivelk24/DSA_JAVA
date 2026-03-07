8a24d447f4a0b2d23e4dfb9baf9337cf0b3d22be
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-03-07 20:41:31 +0530

    Enumeration using Loop

---
+++ AdvancedJava/Enumeration/EnumExample4.java
@@ -0,0 +1,16 @@
+package AdvancedJava.Enumeration;
+
+/* enum - loop*/
+enum Color {
+    RED,
+    GREEN,
+    BLUE;
+}
+
+public class EnumExample4 {
+    public static void main(String[] args) {
+        for (Color c : Color.values()) {
+            System.out.println(c);
+        }
+    }
+}
