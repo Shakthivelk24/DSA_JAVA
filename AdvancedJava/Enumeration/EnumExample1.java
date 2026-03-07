86ac84fec22efafcd5dc6e425c808518ac961285
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-03-07 20:34:44 +0530

    Declaring the enumeration inside the class and outside the class

---
+++ AdvancedJava/Enumeration/EnumExample1.java
@@ -0,0 +1,13 @@
+package AdvancedJava.Enumeration;
+/* enum outside the class*/
+enum Color {
+    RED,
+    GREEN,
+    BLUE;
+}
+public class EnumExample1 {
+    public static void main(String[] args) {
+        Color c = Color.BLUE;
+        System.out.println(c);
+    }
+}
---
+++ AdvancedJava/Enumeration/EnumExample2.java
@@ -0,0 +1,14 @@
+package AdvancedJava.Enumeration;
+
+public class EnumExample2 {
+    enum Color {
+		RED,
+		GREEN,
+		BLUE;
+	}
+    public static void main(String[] args)
+    {
+        Color c = Color.BLUE;
+        System.out.println(c);
+    }
+}
