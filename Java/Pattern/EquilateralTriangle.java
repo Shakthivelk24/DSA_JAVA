d2c8231b212276897d9ac94851fb13d58a2fc04f
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-12 21:14:13 +0530

    Add EquilateralTriangle.java to display an equilateral triangle pattern based on user input

---
+++ Java/Pattern/EquilateralTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(n-i-1);j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }   
+    }
+}
+// 5
+//     * 
+//    * * 
+//   * * * 
+//  * * * * 
+// * * * * * 
